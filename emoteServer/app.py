b135e921e17c59552ad6e406fe54a7209b578732
decafsunrise <danny.cardella@gmail.com>
2023-10-06 02:13:35 -0400

    add emoteserver

--- emoteServer/app.py
+++ emoteServer/app.py
@@ -1,36 +1,59 @@
+from flask import Flask, render_template, request, jsonify
 import os
-from flask import Flask, render_template, request, jsonify, redirect
 
 app = Flask(__name__)
 
-# Configuration file path
-config_file_path = 'config.txt'
+# Directory to store user inputs
+input_dir = os.path.join(os.getcwd(), "data")
 
-# Function to read the current configuration
-def read_config():
-    with open(config_file_path, 'r') as f:
-        return f.read()
+if not os.path.exists(input_dir):
+    os.makedirs(input_dir)
 
-# Function to update the configuration
-def update_config(new_text):
-    with open(config_file_path, 'w') as f:
-        f.write(new_text)
+# Common sidebar data
+sidebar_data = [
+    {"url": "/", "text": "Overview"},
+    {"url": "/system", "text": "System Prompt"},
+    {"url": "/personality", "text": "Personality Prompt"},
+    {"url": "/api/get_inputs", "text": "Check files"}
+]
 
-@app.route('/')
-def index():
-    current_config = read_config()
-    return render_template('index.html', current_config=current_config)
+@app.route("/")
+def home():
+    return render_template("overview.html", sidebar_data=sidebar_data)
 
-@app.route('/update_config', methods=['POST'])
-def update_config_route():
-    new_text = request.form.get('new_config_text')
-    update_config(new_text)
-    return redirect('/')
+@app.route("/system")
+def page1():
+    return render_template("system.html", sidebar_data=sidebar_data)
 
-@app.route('/api/config')
-def get_config_api():
-    current_config = read_config()
-    return jsonify({'config': current_config})
+@app.route("/personality")
+def page2():
+    return render_template("personality.html", sidebar_data=sidebar_data)
 
-if __name__ == '__main__':
+@app.route("/save_input", methods=["POST"])
+def save_input():
+    data = request.json
+    if "filename" in data and "text" in data:
+        filename = data["filename"]
+        text = data["text"]
+        filepath = os.path.join(input_dir, filename)
+
+        with open(filepath, "w") as file:
+            file.write(text)
+        return jsonify({"message": "Input saved successfully!"})
+    else:
+        return jsonify({"error": "Invalid request"}), 400
+
+@app.route("/api/get_inputs")
+def get_inputs():
+    input_files = os.listdir(input_dir)
+    input_data = {}
+
+    for filename in input_files:
+        filepath = os.path.join(input_dir, filename)
+        with open(filepath, "r") as file:
+            input_data[filename] = file.read()
+
+    return jsonify(input_data)
+
+if __name__ == "__main__":
     app.run(debug=True)
